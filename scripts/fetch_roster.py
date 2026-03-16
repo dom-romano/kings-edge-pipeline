7a45525e4877229d5be983a0c57cba5d8539e22b
Dom Romano <domromano2020@gmail.com>
2026-03-16 19:30:51 -0400

    Updated fetch roster script

--- scripts/fetch_roster.py
+++ scripts/fetch_roster.py
@@ -10,9 +10,9 @@ roster = client.teams.team_roster(team_abbr="LAK", season="20252026")
 # Ensure the data directory exists
 os.makedirs('data', exist_ok=True)
 
-# Save the raw data
 file_path = 'data/raw_roster.json'
 with open(file_path, 'w') as f:
     # This saves the whole dictionary (forwards, defense, goalies) in one valid file
-    json.dump(roster, f, indent=4)
-
+    # add to list of players
+    players = roster['forwards'] + roster['defensemen'] + roster['goalies']
+    json.dump(players, f, indent=4)
